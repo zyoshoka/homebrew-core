1f6503a92661f1fe166291c19449afb13942cb97
Michael Cho <michael@michaelcho.dev>
2023-12-08 12:58:34 -0500

    gmic: revert back to cmake build
    * Revert back to cmake build as upstream has restored CMakeLists.txt and
      current build is missing dynamic library after some previous bump.
    * Remove `bash-completion` dependency as that will conflict with
      newer `bash-completion@2` for users who want to use Homebrew `bash`
    
    Signed-off-by: Michael Cho <michael@michaelcho.dev>

--- Formula/g/gmic.rb
+++ Formula/g/gmic.rb
@@ -22,6 +22,7 @@ class Gmic < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a65fcd384a17292da8fe3f60078b5ad037325a8661010a9fded0ad94d024327"
   end
 
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "fftw"
   depends_on "jpeg-turbo"
@@ -32,55 +33,31 @@ class Gmic < Formula
   uses_from_macos "curl"
   uses_from_macos "zlib"
 
-  on_macos do
-    depends_on "bash-completion"
-  end
-
-  # Use .dylibs instead of .so on macOS
-  patch do
-    on_macos do
-      url "https://raw.githubusercontent.com/macports/macports-ports/a859c5929c929548f5156f5cab13a2f341982e72/science/gmic/files/patch-src-Makefile.diff"
-      sha256 "5b4914a05135f6c137bb5980d0c3bf8d94405f03d4e12b6ee38bd0e0e004a358"
-      directory "src"
-    end
-  end
-
   def install
-    # The Makefile is not safe to run in parallel.
-    # Issue ref: https://github.com/dtschump/gmic/issues/406
-    ENV.deparallelize
-
-    # Use PLUGINDIR to avoid trying to create "/plug-ins" on Linux without GIMP.
-    # Disable X11 by using the values from Makefile when "/usr/X11" doesn't exist.
-    args = %W[
-      PLUGINDIR=#{buildpath}/plug-ins
-      USR=#{prefix}
-      X11_CFLAGS=-Dcimg_display=0
-      X11_LIBS=-lpthread
-      SOVERSION=#{version}
+    args = %w[
+      -DENABLE_FFMPEG=OFF
+      -DENABLE_GRAPHICSMAGICK=OFF
+      -DENABLE_X=OFF
     ]
-    system "make", "lib", "cli_shared", *args
-    system "make", "install", *args, "PREFIX=#{prefix}"
-    lib.install "src/libgmic.a"
-
-    if OS.mac?
-      # The Makefile does not install the dylib and gmic.h, so we need to
-      # install them manually.
-      ln_s "libgmic.#{version}.dylib", "src/libgmic.dylib"
-      lib.install "src/libgmic.#{version}.dylib"
-      lib.install "src/libgmic.dylib"
-      include.install "src/gmic.h"
+    if OS.linux?
+      args << "-DENABLE_DYNAMIC_LINKING=ON"
+      ENV.deparallelize
     end
 
-    # Need gmic binary to build completions
-    ENV.prepend_path "PATH", bin
-    system "make", "bashcompletion", *args
-    bash_completion.install "resources/gmic_bashcompletion.sh" => "gmic"
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
     %w[test.jpg test.png].each do |file|
-      system bin/"gmic", test_fixtures(file)
+      # system bin/"gmic", "-verbose", "3", "-debug", test_fixtures(file)
+      pid = fork { exec bin/"gmic", "-verbose", "3", "-debug", test_fixtures(file) }
+      begin
+        sleep 30
+      ensure
+        Process.kill("TERM", pid)
+      end
     end
     system bin/"gmic", "-input", test_fixtures("test.jpg"), "rodilius", "10,4,400,16",
            "smooth", "60,0,1,1,4", "normalize_local", "10,16", "-output", testpath/"test_rodilius.jpg"
