fca1657e3c0d3f6cbfc81fc875dbedff48ebf1bc
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2024-12-20 23:22:13 +0900

    dmgbuild 1.6.2 (new formula)

---
+++ Formula/d/dmgbuild.rb
@@ -0,0 +1,30 @@
+class Dmgbuild < Formula
+  include Language::Python::Virtualenv
+
+  desc "macOS command line utility to build disk images"
+  homepage "https://github.com/dmgbuild/dmgbuild"
+  url "https://files.pythonhosted.org/packages/a9/ce/ac6988d0608c34193502d8069b1277ed849cd7681ea73a994bc715347a42/dmgbuild-1.6.2.tar.gz"
+  sha256 "dba01d29f10c6804f2d72301600ddd03724daa41cd21a95409c44a3e199b19aa"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  resource "ds-store" do
+    url "https://files.pythonhosted.org/packages/7c/36/902259bf7ddb142dd91cf7a9794aa15e1a8ab985974f90375e5d3463b441/ds_store-1.3.1.tar.gz"
+    sha256 "c27d413caf13c19acb85d75da4752673f1f38267f9eb6ba81b3b5aa99c2d207c"
+  end
+
+  resource "mac-alias" do
+    url "https://files.pythonhosted.org/packages/ea/a3/83b50f620d318a98363dc7e701fb94856eaaecc472e23a89ac625697b3ea/mac_alias-2.2.2.tar.gz"
+    sha256 "c99c728eb512e955c11f1a6203a0ffa8883b26549e8afe68804031aa5da856b7"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"dmgbuild", "Test", testpath/"Test.dmg"
+    assert_predicate testpath/"Test.dmg", :exist?
+  end
+end
